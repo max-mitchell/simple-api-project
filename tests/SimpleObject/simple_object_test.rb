0067448bdbae5215fe409e0581306ba4a7df4c89
Max Mitchell <mmitchell4488@yahoo.com>
2020-08-02 17:28:55 -0400

    Start to test

--- tests/SimpleObject/simple_object_test.rb
+++ tests/SimpleObject/simple_object_test.rb
@@ -2,8 +2,29 @@
 require_relative '../test_helper'
 
 class SimpleObjectTest < MiniTest::Unit::TestCase
+
+    def setup
+        @json_requests = [
+            {
+                firstName: "Max",
+                lastName: "Mitchell",
+                dob: "25 June 1903"
+            },
+            {
+                firstName: "Jane",
+                lastName: "Fonda",
+                dob: "7 April 1848"
+            },
+            {
+                firstName: "Mr",
+                lastName: "Man",
+                dob: "1 March 205"
+            }
+        ]
+    end
+
     def test_create
-        simple_object = SimpleObject.create(data: {message: "hello world"})
-        assert_equal 'hello world', simple_object.data["message"]
+        simple_object = SimpleObject.create(data: @json_requests[0])
+        assert_equal JSON.parse(@json_requests[0]), simple_object.data
     end
 end
