91e963d5d47b8c0aba693dd25897ecbe7c582136
Max Mitchell <mmitchell4488@yahoo.com>
2020-08-02 13:21:48 -0400

    Make table name plural

--- db/migrate/20200802161827_create_simple_object.rb
+++ db/migrate/20200802161827_create_simple_object.rb
@@ -1,6 +1,6 @@
 class CreateSimpleObject < ActiveRecord::Migration[6.0]
   def change
-    create_table :simple_object, id: :uuid do |t|
+    create_table :simple_objects, id: :uuid do |t|
       t.json :data
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_161827) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "simple_object", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "simple_objects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.json "data"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
