fecc58928d6f5c446f9cb8b162b8b2a76c1e3d70
Max Mitchell <mmitchell4488@yahoo.com>
2020-08-02 21:55:10 -0400

    Finish routes

--- app/simple_object_api.rb
+++ app/simple_object_api.rb
@@ -17,43 +17,39 @@ class SimpleObjectApi < Sinatra::Base
         # Send an array as json data
         def send_json(data)
             if data.blank?
-                return error_message
+                status 500
             else
                 JSON.pretty_generate(JSON.load(data.to_json))
             end
         end
 
+        # Format SimpleObject, then send
+        def send_simple_object(obj)
+            send_json ["uid" => obj.id, "json-data" => obj.data]
+        end
+
         # Error messages
-        def empty_error
-            status 200
-            send_json :error => "Received no data"
-        end
-
-        def save_error
-            status 200
-            send_json :error => "Failed to save json data"
-        end
-
-        def find_error
-            status 200
-            send_json :error => "No object exists for that uid"
+        def send_error(req, url, msg)
+            status 400
+            send_json :verb => req.request_method, :url => url, :error => msg
         end
     end
 
     # Create route
     post '/api/objects' do
         if !@json_data
-            empty_error
+            send_error request, url, "No data provided."
         else
             # Read json data from request and make a new simple_object
             @simple_object = SimpleObject.create(data: @json_data)
 
             # If the object saves, return the object
             if @simple_object.save
-                send_json @simple_object
+                # Send back data
+                send_simple_object @simple_object
             else
                 # Otherwise, return an error
-                save_error
+                send_error request, url, "Could not save new object."
             end
         end
     end
@@ -61,33 +57,59 @@ class SimpleObjectApi < Sinatra::Base
     # Edit route
     put '/api/objects/:id' do
         if !@json_data
-            empty_error
+            send_error request, url, "No data provided."
         else
             # Find object for given id
             @simple_object = SimpleObject.where(id: params[:id]).first
 
             # If the object can't be found, return an error
             if @simple_object.blank?
-                find_error
+                send_error request, url, "Could not find requested uid."
             else
                 # Otherwise, update and return
                 @simple_object.update(data: @json_data)
-                send_json @simple_object
+
+                # Send back data
+                send_simple_object @simple_object
             end
         end
     end
 
-    # Edit route
+    # Fetch route
     get '/api/objects/:id' do
         # Find object for given id
         @simple_object = SimpleObject.where(id: params[:id]).first
 
         # If the object can't be found, return an error
         if @simple_object.blank?
-            find_error
+            send_error request, url, "Could not find requested uid."
         else
-            # Otherwise return
-            send_json @simple_object
+            # Send back data
+            send_simple_object @simple_object
         end
+    end
+
+    # Fetch all route
+    get '/api/objects' do
+        # Get all objects
+        all_objects = []
+        SimpleObject.all.each do |obj|
+            all_objects << {:url => "#{url}/#{obj.id}"}
+        end
+
+        # Return list of urls
+        send_json all_objects
+    end
+
+    # Delete route
+    delete '/api/objects/:id' do
+        # Find object for given id
+        @simple_object = SimpleObject.where(id: params[:id]).first
+
+        # If the object is real, delete it
+        if !@simple_object.blank?
+            @simple_object.destroy
+        end
+            status 200
     end
 end
