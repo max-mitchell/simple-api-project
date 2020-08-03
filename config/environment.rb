e434bdebbef1eb718c0e4f37afe5c3acd9fb75b2
Max Mitchell <mmitchell4488@yahoo.com>
2020-08-03 14:02:54 -0400

    Move db url to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,12 @@ require'./app/models/simple_object'
 # Set database config
 db_configuration = YAML.load(File.read('./db/config.yml'))
 environment = ENV.fetch('RACK_ENV') { 'development' }
-ActiveRecord::Base.establish_connection(db_configuration[environment])
+db_env = db_configuration[environment]
+
+# If production, include the url here so we
+# don't have to mess with erb
+if environment == "production"
+    db_env[:url] = ENV['DATABASE_URL']
+end
+
+ActiveRecord::Base.establish_connection(db_env)
