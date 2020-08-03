09b8f974bd986fefa12c63dc50e5f5e1ab05df75
Max Mitchell <mmitchell4488@yahoo.com>
2020-08-03 10:20:56 -0400

    Fix test env

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,16 +1,15 @@
 # tests/test_helper.rb
 # Helper for all tests
 
+# Set test env
+ENV['RACK_ENV'] = 'test'
+
 require 'minitest/autorun'
 require 'database_cleaner/active_record'
 require './config/environment'
 require 'rack/test'
 require 'sinatra/base'
 require './app/simple_object_api'
-
-# Set test env
-ENV['RACK_ENV'] = 'test'
-ENV['RAILS_ENV'] = 'test'
 
 # Used to clean db between tests
 DatabaseCleaner.clean_with :deletion
